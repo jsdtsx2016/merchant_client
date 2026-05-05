1083a614cfc24958f65b69e1571de0a915bec3a8
jsdtsx@foxamil.com <jsdtsx@foxamil.com>
2026-05-05 13:11:40 +0800

    1

--- src/layouts/components/Footer/index.vue
+++ src/layouts/components/Footer/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="footer flx-center">
-    <div>2025 © 商户管理系统 v{{ version }}</div>
+    <div>© 商户管理系统 v{{ version }}</div>
   </div>
 </template>
 <script setup lang="ts">
@@ -10,19 +10,19 @@ import { ref, onMounted } from "vue";
 
 const version = ref("1.0.5");
 onMounted(async () => {
-  const result = await checkVersion(version.value, "merchant");
-  if (result.data.reload) {
-    await ElMessageBox.alert(
-      "检测到有新的版本，如遇重复提示请按Ctrl+F5强制刷新！",
-      "发现新版本",
-      {
-        confirmButtonText: "更新系统",
-        type: "warning",
-        showClose: true,
-      },
-    );
-    window.location.reload();
-  }
+  // const result = await checkVersion(version.value, "merchant");
+  // if (result.data.reload) {
+  //   await ElMessageBox.alert(
+  //     "检测到有新的版本，如遇重复提示请按Ctrl+F5强制刷新！",
+  //     "发现新版本",
+  //     {
+  //       confirmButtonText: "更新系统",
+  //       type: "warning",
+  //       showClose: true,
+  //     },
+  //   );
+  //   window.location.reload();
+  // }
 });
 </script>
 <style scoped lang="scss">
